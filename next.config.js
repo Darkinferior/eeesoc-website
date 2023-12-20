bb4b77fa8ebc7dda21732cc1521c82fec560b787
Ved Prakash <vedprakashsigh@gmail.com>
2023-12-20 14:34:19 +0530

    Fresh Start

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: [
-      "miro.medium.com",
-      "images.weserv.nl",
-      "res.cloudinary.com",
-      "www.bitmesra.ac.in",
-    ],
-  },
-  experimental: {
-    serverActions: true,
-  },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- src/utils/getImageSrc.ts
+++
@@ -1,3 +0,0 @@
-export const getImageSrc = (src: string) => {
-  return src[0] === "/" ? `/images${src}` : src;
-};
