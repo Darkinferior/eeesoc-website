7f0ce3720b842d65d2cb445fe7792d72a6c822a3
Aditi-0405 <somyaaditi02@gmail.com>
2024-01-03 22:56:43 +0530

    renamed cloudinary env variables

--- app/api/admin/workshops/addWorkshop/route.ts
+++ app/api/admin/workshops/addWorkshop/route.ts
@@ -11,9 +11,9 @@ import { connectToDb } from "@/lib/dbConnection/connect"
 
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME as string,
-    api_key: process.env.API_KEY as string,
-    api_secret: process.env.API_SECRET as string
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME as string,
+    api_key: process.env.CLOUDINARY_API_KEY as string,
+    api_secret: process.env.CLOUDINARY_API_SECRET as string
 });
 
 export async function POST(request: Request): Promise<NextResponse> {
