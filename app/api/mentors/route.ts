a3528937648baadca0a53de9ce12ab613c89b25c
Aditi-0405 <somyaaditi02@gmail.com>
2024-01-04 16:17:11 +0530

    rectified mentors api

--- app/api/mentors/route.ts
+++ app/api/mentors/route.ts
@@ -1,12 +1,9 @@
-// necessary query parameters = [prefix]
-// optional query parameters = [year]
+// necessary query parameters = []
+// optional query parameters = []
 
 import { connectToDb } from "@/lib/dbConnection/connect"
 import { NextResponse } from "next/server";
-import { CodingInterview } from "@/lib/models/codingInterview"
-import { ConsultancyInterview } from "@/lib/models/consultancyInterview"
-import { ElectronicsInterview } from "@/lib/models/electronicsInterview"
-import { ElectricalInterview } from "@/lib/models/electricalInterviews"
+import { Mentor } from "@/lib/models/mentor"
 
 
 
@@ -14,56 +11,7 @@ import { ElectricalInterview } from "@/lib/models/electricalInterviews"
 export async function GET(request: Request) {
     try {
         await connectToDb();
-        const url = new URL(request.url)
-        const year = url.searchParams.get("year")
-        var prefix = url.searchParams.get("prefix")
-
-        if (prefix) {
-            prefix = prefix.toLowerCase()
-        }
-        var data
-        if (prefix == "coding") {
-            if (year) {
-                data = await CodingInterview.find({ year: year })
-
-
-            }
-            else {
-                data = await CodingInterview.find()
-
-            }
-
-        }
-        else if (prefix == "consultancy") {
-            if (year) {
-                data = await ConsultancyInterview.find({ year: year })
-
-            }
-            else {
-                data = await ConsultancyInterview.find()
-            }
-        }
-        else if (prefix == "electrical") {
-            if (year) {
-                data = await ElectricalInterview.find({ year: year })
-
-            }
-            else {
-                data = await ElectricalInterview.find()
-            }
-        }
-        else if (prefix == "electronics") {
-            if (year) {
-                data = await ElectronicsInterview.find({ year: year })
-
-            }
-            else {
-                data = await ElectronicsInterview.find()
-            }
-        }
-        else {
-            data = "no records found"
-        }
+        const data = await Mentor.find()
 
         return NextResponse.json({ result: data })
     } catch (error) {
