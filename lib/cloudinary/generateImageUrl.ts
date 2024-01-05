0379c54f138be5d73a4eb4d10dcfe48fae85a58d
Aditi-0405 <somyaaditi02@gmail.com>
2024-01-05 12:31:34 +0530

    added cloudinary in lib

---
+++ lib/cloudinary/generateImageUrl.ts
@@ -0,0 +1,34 @@
+import { UploadApiResponse, UploadApiErrorResponse } from 'cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
+import { connectToDb } from "@/lib/dbConnection/connect"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME as string,
+    api_key: process.env.CLOUDINARY_API_KEY as string,
+    api_secret: process.env.CLOUDINARY_API_SECRET as string
+});
+
+export async function uploadImageToCloudinary(image: File, folder: string): Promise<string> {
+    await connectToDb();
+
+    return new Promise<string>(async (resolve, reject) => {
+        const byteData = await image.arrayBuffer();
+        const buffer = Buffer.from(byteData);
+
+        cloudinary.uploader.upload_stream(
+            { folder: folder },  
+            (error: UploadApiErrorResponse | undefined, result: UploadApiResponse | undefined) => {
+                if (error) {
+                    console.error('Error uploading image:', error);
+                    reject(error);
+                } else {
+                    if (result && result.secure_url) {
+                        resolve(result.secure_url);
+                    } else {
+                        reject(new Error('Secure URL is undefined in the upload result.'));
+                    }
+                }
+            }
+        ).end(buffer);
+    });
+}
