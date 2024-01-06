556e16866b1f715fc12527a1efa8f826fd06f722
Ved Prakash <vedprakashsigh@gmail.com>
2024-01-06 14:24:40 +0530

    feat: Edit Alumni to the admin dashboard

--- components/dashboard/EditOurTeam.tsx
+++ components/dashboard/EditOurTeam.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import EditPreFinalYear from './EditOurTeam/EditPreFinalYear';
-import EditFinalYear from './EditOurTeam/EditFinalYear';
+import React from "react";
+import EditPreFinalYear from "./EditOurTeam/EditPreFinalYear";
+import EditFinalYear from "./EditOurTeam/EditFinalYear";
 
 const EditOurTeam = () => {
   return (
     <div>
-      EditOurTeam
       <div>
-        <h1>Edit Final Year</h1>
+        <h1 className='flex items-center justify-center font-bold text-3xl'>
+          Final Year
+        </h1>
         <EditFinalYear />
       </div>
       <div>
-        <h1>Edit Pre-Final Year</h1>
+        <h1 className='flex items-center justify-center font-bold text-3xl'>
+          Pre-Final Year
+        </h1>
         <EditPreFinalYear />
       </div>
     </div>
