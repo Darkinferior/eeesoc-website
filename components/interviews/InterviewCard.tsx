b0173670a413aa9b48cfe0e2c572e80df567b128
Ved Prakash <vedprakashsigh@gmail.com>
2024-01-07 11:21:59 +0530

    fix: Changed Interviews to use NextJs api route to fetch data

--- components/interviews/InterviewCard.tsx
+++ components/interviews/InterviewCard.tsx
@@ -1,38 +1,43 @@
-import { Card, CardHeader, CardBody, CardFooter } from '@nextui-org/card';
-import { Image } from '@nextui-org/image';
-import { Link } from '@nextui-org/link';
-import { Reveal } from '../Reveal';
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+import { Image } from "@nextui-org/image";
+import { Link } from "@nextui-org/link";
+import { Reveal } from "../Reveal";
 
 interface InterviewCardProps {
-  title: string;
+  name: string;
+  company: string;
   link: string;
   image: string;
 }
 
 const InterviewCard: React.FC<InterviewCardProps> = ({
-  title,
+  name,
+  company,
   link,
   image,
 }) => {
   return (
-    <Card className="w-full sm:w-auto md:w-auto lg:w-auto  py-4 items-center justify-center mb-8 transform hover:scale-105 transition-transform hover:shadow-[0_0px_60px_5px_rgba(0.3)] hover:shadow-cyan-500/50">
-      <CardHeader className="pb-0 pt-2 px-4 flex-col">
+    <Card className='w-full sm:w-auto md:w-auto lg:w-auto  py-4 items-center justify-center mb-8 transform hover:scale-105 transition-transform hover:shadow-[0_0px_60px_5px_rgba(0.3)] hover:shadow-cyan-500/50'>
+      <CardHeader className='pb-0 pt-2 px-4 flex-col'>
         <Image
-          alt="Card background"
-          className="object-cover rounded-xl"
+          alt='Card background'
+          className='object-cover rounded-xl'
           src={image}
           width={600}
           height={800}
         />
       </CardHeader>
-      <CardBody className="overflow-visible px-4 py-2 items-center justify-center">
+      <CardBody className='overflow-visible px-4 py-2 items-center justify-center'>
         <Reveal>
-          <h4 className="font-bold text-xl">{title}</h4>
+          <h4 className='font-bold text-xl'>{name}</h4>
+        </Reveal>
+        <Reveal>
+          <h4 className='font-bold text-xl'>{company}</h4>
         </Reveal>
         <Reveal>
           <Link
-            className="px-2 py-4 "
-            color="foreground"
+            className='px-2 py-4 '
+            color='foreground'
             isExternal
             href={link}
             showAnchorIcon
