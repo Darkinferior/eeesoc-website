d998609ceb36878b2fad6b652e10edfa701250b4
Ved Prakash <vedprakashsigh@gmail.com>
2024-01-13 19:30:52 +0530

    fix: merge conflict error fixed

--- components/dashboard/EditHome/EditRegisterBtn.tsx
+++ components/dashboard/EditHome/EditRegisterBtn.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, ChangeEvent } from 'react';
-import { Button, Input, Card } from '@nextui-org/react';
+import React, { useState, useEffect, ChangeEvent } from "react";
+import { Button, Input, Card } from "@nextui-org/react";
 
 interface BtnData {
   title: string;
@@ -8,8 +8,8 @@ interface BtnData {
 
 const EditRegisterBtn: React.FC = () => {
   const [BtnData, setBtnData] = useState<BtnData>({
-    title: '',
-    formLink: '',
+    title: "",
+    formLink: "",
   });
 
   const [editBtnId, setEditBtnId] = useState<string | null>(null);
@@ -29,20 +29,16 @@ const EditRegisterBtn: React.FC = () => {
 
   const fetchBtn = async () => {
     try {
-      const response = await fetch('/api/events/register');
+      const response = await fetch("/api/events/register");
       if (response.ok) {
         const data = await response.json();
-        if (Array.isArray(data.result)) {
-          setBtnData(data.result);
-          setEditBtnId(data.result._id);
-        } else {
-          console.error('Btn data is not an array:', data);
-        }
+        setBtnData(data.result);
+        setEditBtnId(data.result._id);
       } else {
-        console.error('Failed to fetch Btn');
+        console.error("Failed to fetch Btn");
       }
     } catch (error) {
-      console.error('Error fetching Btn:', error);
+      console.error("Error fetching Btn:", error);
     }
   };
 
@@ -55,7 +51,7 @@ const EditRegisterBtn: React.FC = () => {
     try {
       const url = `/api/admin/events/register?id=${id}`;
       const response = await fetch(url, {
-        method: 'PUT',
+        method: "PUT",
         body: formData,
       });
 
@@ -63,47 +59,47 @@ const EditRegisterBtn: React.FC = () => {
         fetchBtn();
 
         setBtnData({
-          title: '',
-          formLink: '',
+          title: "",
+          formLink: "",
         });
 
         setEditBtnId(null);
       } else {
-        console.error('Failed to edit Btn');
+        console.error("Failed to edit Btn");
       }
     } catch (error) {
-      console.error('Error editing Btn:', error);
+      console.error("Error editing Btn:", error);
     }
   };
 
   return (
     <div>
-      <Card isBlurred className="mt-4 mb-4">
+      <Card isBlurred className='mt-4 mb-4'>
         <form onSubmit={() => handlePutBtn(editBtnId as string)}>
-          <div className="mt-4 mb-4">
+          <div className='mt-4 mb-4'>
             <Input
               isRequired
-              type="text"
-              name="title"
-              label="Enter Title"
+              type='text'
+              name='title'
+              label='Enter Title'
               value={BtnData.title}
               onChange={handleInputChange}
               required
             />
           </div>
-          <div className="mt-4 mb-4">
+          <div className='mt-4 mb-4'>
             <Input
               isRequired
-              type="text"
-              name="formLink"
-              label="Enter Form Link"
+              type='text'
+              name='formLink'
+              label='Enter Form Link'
               value={BtnData.formLink}
               onChange={handleInputChange}
               required
             />
           </div>
 
-          <Button type="submit">Save</Button>
+          <Button type='submit'>Save</Button>
         </form>
       </Card>
     </div>
