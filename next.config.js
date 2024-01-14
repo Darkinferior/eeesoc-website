7c8954ef5b26db4e722c3ce086343bc4d2dd73df
shivesh-anand <btech10218.21@bitmesra.ac.in>
2024-01-14 19:07:49 +0530

    console fixes, interview card fixes

--- next.config.js
+++ next.config.js
@@ -3,11 +3,4 @@ const nextConfig = {};
 
 module.exports = {
   reactStrictMode: true,
-
-  head: {
-    themeColor: [
-      { media: '(prefers-color-scheme: light)', color: 'white' },
-      { media: '(prefers-color-scheme: dark)', color: 'black' },
-    ],
-  },
 };
